6d8c7d8c5e1b0ac47cb162e97567b133869379df
lanyubaihua <3345568463@qq.com>
2022-09-21 22:57:29 +0800

    初始化

---
+++ src/Test/solution.java
@@ -0,0 +1,11 @@
+
+import java.util.HashMap;
+
+public class solution {
+   public static void main(String[] args) {
+    HashMap<String,Integer>hashMap=new HashMap<>(10);
+   hashMap.put("song",123);
+
+   }
+
+}
