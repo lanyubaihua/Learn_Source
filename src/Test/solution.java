d27dc0cd6658c83b357ae7334906ca7715dc87b9
lanyubaihua <3345568463@qq.com>
2023-05-21 23:48:58 +0800

    Add

--- src/Test/solution.java
+++ src/Test/solution.java
@@ -1,11 +1,6 @@
-
-import com.sun.org.apache.bcel.internal.generic.RET;
-import org.omg.CORBA.INTERNAL;
-
-import javax.print.attribute.standard.Media;
-
 class solution {
     public static void main(String[] args) {
         new methodA().say();
+        System.out.println("fada");
     }
 }
